aa463c741db8990da16ce06698dda83d36621bc0
Henrique Queiroz Reuter <henriquereuter46@gmail.com>
2025-04-08 20:09:46 -0300

    Função que normaliza os números de telefone usando regex

--- main.go
+++ main.go
@@ -1,19 +1,28 @@
 package main
 
-import "bytes"
+import (
+	"regexp"
+)
 
+//func normalize(telefone string) string {
+// Queremos o telefone nesse modelo - 012345678
+
+//var buf bytes.Buffer
+
+//for _, ch := range telefone {
+//0 - 9 na tabela ascii estão entre os índices 0 e 9
+//if ch >= '0' && ch <= '9' {
+//buf.WriteRune(ch)
+//}
+
+//}
+//return buf.String()
+
+//}
+
+// Também é uma função que normaliza um número de telefone, mas usa regex
 func normalize(telefone string) string {
-	// Queremos o telefone nesse modelo - 012345678
-
-	var buf bytes.Buffer
-
-	for _, ch := range telefone {
-		//0 - 9 na tabela ascii estão entre os índices 0 e 9
-		if ch >= '0' && ch <= '9' {
-			buf.WriteRune(ch)
-		}
-
-	}
-	return buf.String()
+	re := regexp.MustCompile("[^0-9]")
+	return re.ReplaceAllString(telefone, "")
 
 }
